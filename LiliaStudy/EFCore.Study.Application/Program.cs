3d21e41267cf5f8ea15c3db967aa4345ec5b7ebd
Pavlo <Ramracing1@ukr.net>
2024-02-28 12:21:05 +0200

    1

---
+++ LiliaStudy/EFCore.Study.Application/Program.cs
@@ -0,0 +1,31 @@
+﻿//Download install https://marketplace.visualstudio.com/items?itemName=ErikEJ.SQLServerCompactSQLiteToolbox
+//Close visual studio to continue installation
+//Search for sqllite/SQL Server compact toolbox
+//Right mouse click -> Add SQLLite Connection -> Find Db file in EFCore.Study.Application bin folder with other dll files.
+using EFCore.Study.Domain.Models;
+using EFCore.Study.Infrastructure.DB;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    // создаем два объекта User
+    User tom = new User { Name = "Tom", Age = 33 };
+    User alice = new User { Name = "Alice", Age = 26 };
+    User pasha = new User { Name = "Pasha", Age = 33 };
+    User lilia = new User { Name = "Lilia", Age = 31 };
+
+    // добавляем их в бд
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    db.Users.Add(pasha);
+    db.Users.Add(lilia);
+    db.SaveChanges();
+    Console.WriteLine("Объекты успешно сохранены");
+
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
---
+++ LiliaStudy/EFCore.Study.Infrastructure.DB/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using EFCore.Study.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Study.Infrastructure.DB
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
