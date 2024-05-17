f6647de73d921637b99a0caf367f9eba958c40d6
Pavlo <Ramracing1@ukr.net>
2024-05-17 12:05:32 +0300

    2

---
+++ LiliaStudy/CaloriesCalculator.InterfaceRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace CaloriesCalculator.Interface.Repository
+{
+    public interface IRepository<T>
+    {
+        int Create(T entity);
+        void Update(int id, T entity);
+        void Delete(params int[] id);
+        T Get(int id);
+        IEnumerable<T> GetAll();
+
+    }
+}
