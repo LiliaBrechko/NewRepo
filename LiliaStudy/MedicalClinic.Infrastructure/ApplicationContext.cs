d03754afcaf2127dc318b0ed163e57ae0805095c
Pavlo <Ramracing1@ukr.net>
2024-06-22 23:03:40 +0300

    Second Add migration

---
+++ LiliaStudy/MedicalClinic.Infrastructure/ApplicationContext.cs
@@ -0,0 +1,39 @@
+﻿using MedicalClinic.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace MedicalClinic.Infrastructure
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; } = null!;
+        public DbSet<Patient> Patients { get; set; } = null!;
+        public DbSet<Appointment> Appointments { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.
+                GetCurrentDirectory()).AddJsonFile("ApplicationSetting.json").Build();
+            optionsBuilder.UseSqlite(builder.GetConnectionString("DefaultConnection"));
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>()
+            .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Doctor)
+                .WithMany(d => d.Appointments)
+                .HasForeignKey(a => a.DoctorId);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Patient)
+                .WithMany(p => p.Appointments)
+                .HasForeignKey(a => a.PatientId);
+        }
+
+    }
+}
