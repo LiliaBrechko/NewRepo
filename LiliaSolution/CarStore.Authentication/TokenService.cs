c21a897ffb4f3a15edc6a76c589a88e78af478ac
Pavlo <Ramracing1@ukr.net>
2024-11-27 16:31:25 +0200

    Clean Architecture with Bearer Auth

---
+++ LiliaSolution/CarStore.Authentication/TokenService.cs
@@ -0,0 +1,31 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CarStore.Authentication;
+public class TokenService
+{
+    public const string SecretKey = "45dc083f-09db-420e-be59-0917d7ce789a";
+    private const int ExpirationMinutes = 30;
+
+    public static string GenerateToken(string username)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(SecretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            }),
+            Expires = DateTime.UtcNow.AddMinutes(ExpirationMinutes),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
---
+++ LiliaSolution/CarStore.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using CarStore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarStore.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        var token = TokenService.GenerateToken(request.Username);
+
+        return Ok(new { Token = token });
+
+        //if (request.Username == "test" && request.Password == "password")
+        //{
+        //    var token = TokenService.GenerateToken(request.Username);
+        //    return Ok(new { Token = token });
+        //}
+
+        //return Unauthorized("Invalid credentials");
+    }
+
+    [HttpGet("protected")]
+    [Authorize]
+    public IActionResult Protected()
+    {
+        var username = User.Identity?.Name; // Доступ к данным из токена
+        return Ok($"Hello, {username}! This is a protected endpoint.");
+    }
+}
+
+public class LoginRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
